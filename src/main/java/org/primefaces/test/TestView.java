38ee3b47fa05d6ad1a409eac5972091b3140a094
arn95mr <56418197+arn95mr@users.noreply.github.com>
2019-10-15 10:30:02 -0600

    Add files via upload

---
+++ src/main/java/org/primefaces/test/TestView.java
@@ -0,0 +1,28 @@
+package org.primefaces.test;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class TestView implements Serializable {
+    
+    private String testString;
+    
+    @PostConstruct  
+    public void init() {
+        testString = "Welcome to PrimeFaces!!!";
+    }
+    
+    
+    //Esto es un comentario
+    public String getTestString() {
+        return testString;
+    }
+
+    public void setTestString(String testString) {
+        this.testString = testString;
+    }    
+}
